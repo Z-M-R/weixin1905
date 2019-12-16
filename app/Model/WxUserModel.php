91a11baa2bd36dab9b431f89a404a0731cbeb794
Z-M-R <2417894482@qq.com>
2019-12-16 17:26:21 +0800

    用户管理

--- app/Model/WxUserModel.php
+++ app/Model/WxUserModel.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class WxUserModel extends Model
 {
     // 关注者的数据表
-    public $table = 'p_wx_users';
+    protected $table = 'p_wx_users';
     protected $primaryKey = 'uid';
 }
