90ab00c64b73cd771afae3497cc5814eab6a3639
Z-M-R <2417894482@qq.com>
2019-12-18 18:00:07 +0800

    shangchuan

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -35,7 +35,7 @@ class WxController extends Controller
             return $access_token;
         }
 
-        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSWCRET');
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET');
         $data_json = file_get_contents($url);
         $arr = json_decode($data_json,true);
 
