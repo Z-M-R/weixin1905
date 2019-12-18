a219c4544975f30f399021442816ba61eb9c0138
Z-M-R <2417894482@qq.com>
2019-12-18 14:13:27 +0800

    投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class VoteController extends Controller
 {
@@ -13,9 +14,16 @@ class VoteController extends Controller
         $code = $_GET['code'];
 
         //获取access_token
+        // TODO 判断是否已经投过 使用redis 集合 或有序集合
+
         $data = $this->getAccessToken($code);
         //获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
+
+        // 处理业务逻辑
+        $redis_key = 'vote';
+        $number = Redis::incr($redis_key);
+        echo "投票成功，当前票数：" . $number;
 
     }
 
@@ -35,7 +43,12 @@ class VoteController extends Controller
     {
         $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $json_data = file_get_contents($url);
-        $user_info = json_decode($json_data,true);
-        echo '<pre>';print_r($user_info);echo '</pre>';
+        $data = json_decode($json_data,true);
+        if(isset($data['errcode'])){
+            //TODO 错误处理
+            die("出错了 40001");        // 40001 标识获取用户信息失败
+        }
+
+        return $data;       // 返回用户信息
     }
 }
