2923ba4de38148d53f6879424dd52c1dd2bebdca
Your Name <you@example.com>
2019-12-20 06:57:08 +0000

    商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,5 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource('users', WxUserController::class);
     $router->resource('wxmsg', WxMsgController::class);
+    $router->resource('wxgoods', GoodsController::class);
 });
