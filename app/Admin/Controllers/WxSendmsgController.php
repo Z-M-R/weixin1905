918bb59ff4009756a1347214c2952b1a6741fb08
Your Name <you@example.com>
2019-12-21 02:32:39 +0000

    计划任务

--- app/Admin/Controllers/WxSendmsgController.php
+++ app/Admin/Controllers/WxSendmsgController.php
@@ -32,8 +32,20 @@ class WxSendmsgController extends AdminController
 
         $url='https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token='.$this->access_token.'';
 
+        // 获取天气
+        $weather_api = 'https://free-api.heweather.net/s6/weather/now?location=beijing&key=0f389fa62c0f4c4f89276a68a02bb530';
+            $weather_info = file_get_contents($weather_api);
+            $weather_info_arr = json_decode($weather_info,true);
+            // echo '<pre>';print_r($weather_info_arr);echo '</pre>';
 
-        $msg = date('Y-m-d H:i:s').'发送消息';
+            $cond_txt = $weather_info_arr['HeWeather6'][0]['now']['cond_txt'];
+            $tmp = $weather_info_arr['HeWeather6'][0]['now']['tmp'];
+            $wind_dir = $weather_info_arr['HeWeather6'][0]['now']['wind_dir'];
+
+            $msg = $cond_txt . '温度:' . $tmp . '风向:' . $wind_dir;
+
+
+        $msg = date('Y-m-d H:i:s') . $msg ;
 
 
         $data=[
