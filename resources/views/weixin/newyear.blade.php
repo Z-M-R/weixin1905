82d95e0e4f49fe47e2b73e0541fc53be11e01d14
Your Name <you@example.com>
2019-12-24 04:46:39 +0000

    活动

--- resources/views/weixin/newyear.blade.php
+++ resources/views/weixin/newyear.blade.php
@@ -22,6 +22,35 @@
               jsApiList: ['updateAppMessageShareData','updateTimelineShareData'] // 必填，需要使用的JS接口列表
             });
             
+
+            wx.ready(function(){
+             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+            
+            //发给朋友
+            wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+                wx.updateAppMessageShareData({ 
+                title: '元旦活动', // 分享标题
+                desc: '元旦促销', // 分享描述
+                link: 'http://weixin1905.zmrzzj.com/wx/newyear', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://weixin1905.zmrzzj.com/img/2ha.jpg', // 分享图标
+                success: function () {
+                    // 设置成功
+                }
+                })
+            });
+            
+            //分享到朋友圈
+            wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
+            wx.updateTimelineShareData({ 
+                title: '走过路过不要错过', // 分享标题
+                link: 'http://weixin1905.zmrzzj.com/wx/newyear', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://weixin1905.zmrzzj.com/img/2ha.jpg', // 分享图标
+                success: function () {
+                    // 设置成功
+                }
+            })
+            });
+            });
         </script>
 
 
