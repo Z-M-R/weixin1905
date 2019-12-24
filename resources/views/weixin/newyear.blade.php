1b29786ff5ecab56ebf8e908034989e879e453f1
Your Name <you@example.com>
2019-12-24 03:41:34 +0000

    修改

--- resources/views/weixin/newyear.blade.php
+++ resources/views/weixin/newyear.blade.php
@@ -22,7 +22,6 @@
               jsApiList: ['updateAppMessageShareData','updateTimelineShareData'] // 必填，需要使用的JS接口列表
             });
             wx.ready(function(){
-              // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
                 // 发送给朋友
                 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
                   wx.updateAppMessageShareData({ 
